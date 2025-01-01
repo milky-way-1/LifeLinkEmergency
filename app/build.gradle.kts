f295f0a14b342affbcbe83d1b4d21b03628c6e7c
milky-way-1 <shettysuhassc@gmail.com>
2025-01-02 04:18:44 +0530

    Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,12 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        android.buildFeatures.buildConfig = true
+        val secretsFile = rootProject.file("local.properties")
+        val properties = project.properties
+        buildConfigField("String", "MAPS_API_KEY", "\"${properties["MAPS_API_KEY"]}\"")
+        manifestPlaceholders["MAPS_API_KEY"] = properties["MAPS_API_KEY"].toString()
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -45,4 +51,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation("tech.gusavila92:java-android-websocket-client:1.2.2")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
 }
